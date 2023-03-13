85c6b142689df6e7050c03fcb77fb16d716d0541
henriquekk <henrique.guerrato@gmail.com>
2023-03-13 13:36:23 -0300

    FILTRO JS

--- js/filtro-produtos.js
+++ js/filtro-produtos.js
@@ -1,26 +1,20 @@
 let botaoFiltrar = document.getElementById("botao-filtro");
 let limparFiltro = document.querySelector("#botao-limpar-filtro");
-let produtos = document.getElementsByClassName(".caixa-pagina-produtos");
+
 
 function pegarValores(){
     let categoria = document.getElementsByName("categorias");
-
+    let produtos = document.querySelectorAll(".caixa-pagina-produtos");
 
     for(var i=0; i<categoria.length; i++){
         if(categoria[i].checked){
             console.log("a categoria selecionada é: "+ categoria[i].value)
         }
          else{
-            produtos.classList.add(".filtro-hide")
+            produtos[i].classList.add("filtro-hide")
         }
     }    
 }
-
-
-
-
-
-
 
 botaoFiltrar.addEventListener("click", pegarValores);
 
